4007fe29664d4b6c015407706f9f94147304ddad
Abhishek verma <86115498+AV311241@users.noreply.github.com>
2023-10-29 22:31:07 +0530

    Add files via upload

--- static/script/app.js
+++ static/script/app.js
@@ -1,16 +1,7 @@
-// function showPreviewImage() {
-//     var previewImage = document.getElementById('preview-image');
-//     previewImage.src = document.querySelector('.my-image').src;
-//     document.getElementById('image-previewer').style.display = 'block';
-//   }
-  
-//   document.getElementById('image-previewer').onclick = function() {
-//     this.style.display = 'none';
-//   };
-
-function showPreviewImage() {
+function showPreviewImage(e) {
     var previewImage = document.getElementById('preview-image');
-    previewImage.src = event.target.src;
+    previewImage.src = e.target.src;
+    document.getElementById('img_url_name').value = e.target.src;
     document.getElementById('image-previewer').style.display = 'block';
    
     document.getElementById('image-previewer').onclick = function() {
@@ -21,3 +12,36 @@ function showPreviewImage() {
   document.querySelectorAll('.my-image').forEach(function(image) {
     image.addEventListener('click', showPreviewImage);
   });
+
+
+
+
+  /* validate */ 
+
+  function validateUsername(username) {
+    if (username.length < 8) {
+      return false;
+    }
+    const specialCharacters = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,<>\/?~]/;
+    if (specialCharacters.test(username)) {
+      return false;
+    }
+    // If the username passes all of the validation checks, return true.
+    return true;
+  }
+  
+
+const username = document.getElementById('username');
+const alert_box = document.getElementById('alert-box-validation')
+
+// Validate the username when the user types in the input field.
+username.addEventListener('input', () => {
+  const isValid = validateUsername(username.value);
+  if(isValid){
+    alert_box.innerHTML = ""
+  }
+  if(!isValid){
+    alert_box.innerHTML = "Avoid using special character"
+  }
+});
+
